669e2cafc64f86cf6f312a9624da23a8039428f5
akyravish <info.akshayravish@gmail.com>
2021-06-10 11:33:56 +0530

    add js code

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const toggles = document.querySelectorAll('.toggle');
+const good = document.getElementById('good');
+const cheap = document.getElementById('cheap');
+const fast = document.getElementById('fast');
 
+const doMagic = (change) => {
+	if (good.checked && cheap.checked && fast.checked) {
+		if (good === change) {
+			fast.checked = false;
+		}
+
+		if (cheap === change) {
+			good.checked = false;
+		}
+
+		if (fast === change) {
+			cheap.checked = false;
+		}
+	}
+};
+
+toggles.forEach((toggle) =>
+	toggle.addEventListener('change', (e) => {
+		doMagic(e.target);
+	})
+);
